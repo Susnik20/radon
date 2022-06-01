ba93ea4a4b7a26fbd9c7b46dd3e61d8342285382
Susnik20 <anikh725@gmail.com>
2022-06-01 16:04:14 +0530

    session/nodejs-intro

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
+const logger = require('./logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    console.log('i am anik halder'+ logger.url)
+    logger.log()
     res.send('My first ever api!')
 });
+router.get('/test-me1', function (req, res) {
+    res.send('My second ever api!')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('My last api!')
+});
+
 
 module.exports = router;
 // adding this comment for no reason
