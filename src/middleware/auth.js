194e7919c15344153fdc20faca09b23417d10669
Susnik20 <anikh725@gmail.com>
2022-06-17 00:08:30 +0530

    anikHalder

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,40 @@
-const authenticate = function(req, req, next) {
+const jwt = require('jsonwebtoken')
+const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
+    try{
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.status(401).send({status : false ,msg : "token is not present in the headers"});
 
-    next()
+    const decodedToken = jwt.verify(token,"functionup-radon")
+    if (!decodedToken) return res.status(401).send({status : false , msg:"token is not valid"});
+    }catch (err){
+        res.status(401).send(err.message)
+    }
+    next();
 }
 
 
 const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
+    try{
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    const decodedToken = jwt.verify(token,"functionup-radon")
+    let userToBeModified = req.params.userId
+   
+    let userLoggedIn = decodedToken.userId
+
+    
+    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+    }catch(err){
+        res.status(403).send(err.message)
+    }
+    next();
 }
+
+
+
+
+module.exports.authenticate = authenticate;
+module.exports.authorise = authorise;
